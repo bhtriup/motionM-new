1f85a5bb225ea893bc22a569738a72466397b5a0
ejjung <ejjung@triupcorp.com>
2023-02-09 15:02:11 +0900

    조직도 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import {typeOrmConfig} from "./configs/typeorm.config";
 import { UsersModule } from './users/users.module';
+import { OrganizationModule } from './organization/organization.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { UsersModule } from './users/users.module';
     }),
     TypeOrmModule.forRoot(typeOrmConfig),
     UsersModule,
+    OrganizationModule,
   ],
   controllers: [],
   providers: [],
