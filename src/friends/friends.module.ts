51a2c6197c625d677bc86d905da5deab3f946d89
ejjung <ejjung@triupcorp.com>
2023-02-10 18:39:25 +0900

    친구리스트 작업

--- src/friends/friends.module.ts
+++ src/friends/friends.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { FriendsController } from './friends.controller';
 import { FriendsService } from './friends.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../users/entity/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [FriendsController],
-  providers: [FriendsService]
+  providers: [FriendsService],
 })
 export class FriendsModule {}
